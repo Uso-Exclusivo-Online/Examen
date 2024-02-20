f74c2c5348e94c832c6fe5a4cb57a22bd75289f5
Uso-Exclusivo-Online <144046067+Uso-Exclusivo-Online@users.noreply.github.com>
2024-02-20 13:09:53 -0600

    Add files via upload

---
+++ examen/examen.js
@@ -0,0 +1,21 @@
+
+const arreglo =[{h1:'Martillo',h2:'Taladro',h3:'Desarmador',h4:'Clavo',h5:'Cuerda'}];
+
+//Funcion Recorrer
+console.log("---------------");
+const aaa = arreglo.map;
+console.log(aaa);
+
+
+
+
+//DESESTRUCTURACION ->
+
+console.log("---------------");
+let [he1,he2,he3,he4,he5] = arreglo;
+
+console.log ("Herramientas: " + he1.h1);
+console.log ("Herramientas: " + he1.h2);
+console.log ("Herramientas: " + he1.h3);
+console.log ("Herramientas: " + he1.h4);
+console.log ("Herramientas: " + he1.h5);
